0013ed8abafdeadc0ac037503aa8c2e8ad808fa4
kevinaltschuler <kevinaltschuler@gmail.com>
2017-11-08 17:31:28 -0500

    a warning

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase'
+
+// if u do nefarious things with this ill send my cronies after you
 const config = {
     apiKey: "AIzaSyDxyPKxO-qIlPOMe_JD5IIq4PLaSUfC51E",
     authDomain: "battle-clicker.firebaseapp.com",
